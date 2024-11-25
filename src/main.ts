2cfa543817d47a7f3c7d1930f49874eed7da1b0d
Wesley <wesley.bento@capgemini.com>
2024-11-25 13:56:43 -0300

    ajustes

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,30 @@
-import app from "./routes";
-import { Server, Socket } from "socket.io";
-import http from "http";
+import app from "./routes";  // Importa o app do arquivo routes.ts
+import { Server, Socket } from "socket.io";  // Importa classes do Socket.IO
+import http from "http";  // Importa o módulo http para criar o servidor
 
+// Cria o servidor HTTP utilizando o Express
 const serv = http.createServer(app);
 
+// Cria a instância do Socket.IO associada ao servidor HTTP
 const ws = new Server(serv);
 
-ws.of("/foo").on("connection", (ws: Socket) => {
-  ws.on("join", (data: any, callback) => {
-    callback(data)
+// Configura o namespace '/foo' para aceitar conexões WebSocket
+ws.of("/foo").on("connection", (socket: Socket) => {
+  console.log('Usuário conectado a /foo');
+
+  // Configura o evento 'join' para responder com os dados recebidos
+  socket.on("join", (data: any, callback) => {
+    console.log("Recebido no join:", data);
+    callback(data);  // Responde com o mesmo dado recebido
+  });
+
+  // Detecta quando o usuário se desconecta
+  socket.on("disconnect", () => {
+    console.log('Usuário desconectado');
   });
 });
 
-
-serv.listen(3000, ()=>{
-    console.log('servidor instanciado.')
-})
+// Inicia o servidor HTTP na porta 3000
+serv.listen(3000, () => {
+  console.log('Servidor WebSocket e Express instanciado na porta 3000.');
+});
--- src/routes.ts
+++ src/routes.ts
@@ -1,19 +1,29 @@
 import express, { Router, Response, Request } from "express";
-import { io } from "socket.io-client";
+import { io } from "socket.io-client";  // Cliente do Socket.IO para conectar ao servidor WebSocket
 
-//conecta com o websocket 
+// Conectando-se ao servidor WebSocket na rota '/foo'
 const ws = io("http://localhost:3000/foo");
 
 const app = express();
-
 const router = Router();
 
+// Rota Express que dispara um evento WebSocket
 router.get("/hc/:id", (req: Request, res: Response) => {
-  ws.emit("join", req.params.id, (response: any)=>{
-    res.json({message: response })
-  } );
+  const { id } = req.params;
+
+  // Emitindo o evento 'join' para o servidor WebSocket com o ID
+  ws.emit("join", id, (response: any) => {
+    // Responde ao cliente HTTP com a resposta recebida do WebSocket
+    res.json({ message: `Resposta do WebSocket: ${response}` });
+  });
 });
 
-app.use('/test',router);
+// Usando o router no servidor Express
+app.use('/test', router);
 
-export default app;
+// Iniciando o servidor Express na porta 4000
+app.listen(4000, () => {
+  console.log("Servidor Express rodando na porta 4000.");
+});
+
+export default app
